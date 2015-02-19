b7d5b12f0e55b7eda9ba3cb7603ba2171fd32e1a
Mathieu Dutour <mathieu@dutour.me>
2015-02-19 23:31:12 +0000

    add dimmer

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'matdutour:popup-confirm',
-  version: '0.1.13',
+  version: '0.1.14',
   summary: 'A clean and easy to use confirmation popup',
   git: 'https://github.com/mathieudutour/Meteor-popup-confirm.git',
   documentation: 'README.md'
