b30786a4ef33081813fc3c7061ef31a51bf8c6b8
Mathieu Dutour <mathieu@dutour.me>
2016-09-10 21:39:00 +0100

    allow html in message

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'matdutour:popup-confirm',
-  version: '0.1.26',
+  version: '0.2.0',
   summary: 'A clean and easy to use confirmation popup',
   git: 'https://github.com/mathieudutour/Meteor-popup-confirm.git',
   documentation: 'README.md'
