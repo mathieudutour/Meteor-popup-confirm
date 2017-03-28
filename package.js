c1ac7758baaba8f56f4e00c7305f6840e1fae8d3
Mathieu Dutour <mathieu@dutour.me>
2017-03-28 09:35:52 +0100

    version 0.2.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'matdutour:popup-confirm',
-  version: '0.2.1',
+  version: '0.2.2',
   summary: 'A clean and easy to use confirmation popup',
   git: 'https://github.com/mathieudutour/Meteor-popup-confirm.git',
   documentation: 'README.md'
